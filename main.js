63b5d79b183a7618577b5c0464f94d8842f0b977
ednukros <isanmenarguez@gmail.com>
2022-12-24 15:47:32 +0100

    Iteration-1

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+// evento click que ejecute un console log con la información del evento del click
+
+function clickMe (){
+    console.log("Has hecho click!");
+}
+
+document.getElementById("btnToClick").onclick = clickMe;
+
+// 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+
+// 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input
+
